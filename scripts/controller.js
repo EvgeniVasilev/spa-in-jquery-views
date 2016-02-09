e4ce4358e8206161ddac63aa49e5b0130fbcafd7
Evgeni Vasilev <evgeni.vasilev0912@gmail.com>
2016-02-09 18:34:39 +0200

    Add bookmark capability to controller.

--- scripts/controller.js
+++ scripts/controller.js
@@ -4,20 +4,32 @@ function makeAjaxCall(ajaxParam) {
 
 $(document).ready(function () {
 
+
     $.getJSON("route.json", function (params) {
         for (var i = 0; i < params.length; i++) {
             $("#nav").append("<a  id='nav" + (i) + "'>" + params[i].name + "</a>&nbsp;");
-            
+
             $("#nav" + (i)).attr("href", "#" + params[i].url);
-            
-             $("#nav" + (i)).attr("data-view",  params[i].view_url);
-             
-              $("#nav" + (i)).click(function() {
-                  makeAjaxCall($(this).attr("data-view"));
-              });
+
+            $("#nav" + (i)).attr("data-view", params[i].view_url);
+
+            $("#nav" + (i)).click(function () {
+                makeAjaxCall($(this).attr("data-view"));
+            });
         }
-               
-         $("#container").load("./views/" + params[0].view_url)
-    });   
-    // makeAjaxCall(null);
+    });
+
+    window.addEventListener("load", function () {
+        var my_path = window.location.hash;
+        if (my_path !== "#new_zealand" && my_path !== "#bulgaria") {
+            $("#container").load("./views/australia.html");
+        } else if (my_path === "#new_zealand") {
+            $("#container").load("./views/new_zealand.html");
+        } else if (my_path === "#bulgaria") {
+            $("#container").load("./views/bulgaria.html");
+        } else if (my_path === "#australa") {
+            $("#container").load("./views/australi.html");
+        }
+    }, false);
+
 });
